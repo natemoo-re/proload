af73fce114be83049978838c8b30e95f94dbb228
Nate Moore <nate@natemoo.re>
2021-02-10 22:10:10 -0600

    fix: add `default` fallback to CJS file

--- packages/core/lib/cjs/index.cjs
+++ packages/core/lib/cjs/index.cjs
@@ -1,3 +1,6 @@
-module.exports = (...args) => {
-    return import('../esm/index.mjs').then(({ default: mdl }) => mdl(...args))
-};
+function load(...args) {
+    return import('../esm/index.mjs').then(({ default: loader }) => loader(...args));
+}
+
+load.default = load;
+module.exports = load;
