5bc6a51728ba2f4bc71619e50923f44d1bcc3c55
Nate Moore <nate@natemoo.re>
2021-02-10 22:09:55 -0600

    docs: improve inline documentation in types

--- packages/core/lib/index.d.ts
+++ packages/core/lib/index.d.ts
@@ -1,19 +1,45 @@
 export interface Config<T> {
+    /** An absolute path to a resolved configuration file */
     filePath: string;
+    /** The raw value of a resolved configuration file, before being merged with any `extends` configurations */
     raw: any;
+    /** The final, resolved value of a resolved configuration file */
     value: T;
 }
 
 export interface LoadOptions<T> {
+    /** 
+      * The location from which to begin searching up the directory tree 
+      *
+      * [Read the `@proload/core` docs](https://github.com/natemoo-re/proload/tree/main/packages/core#cwd)
+      */
     cwd?: string;
+    /** 
+      * If a resolved configuration file exports a factory function, this value will be passed as arguments to the function
+      *
+      * [Read the `@proload/core` docs](https://github.com/natemoo-re/proload/tree/main/packages/core#context)
+      */
     context?: any;
+    /**
+      * A function to customize the `merge` behavior when a config with `extends` is encountered 
+      *
+      * [Read the `@proload/core` docs](https://github.com/natemoo-re/proload/tree/main/packages/core#merge)
+      */
     merge?(x: Partial<T>, y: Partial<T>): Partial<T>;
+    /** 
+      * A function to completely customize module resolution
+      *
+      * [Read the `@proload/core` docs](https://github.com/natemoo-re/proload/tree/main/packages/core#accept)
+      */
     accept?(fileName: string, context: { directory: string }): boolean|void;
 }
 
 export interface Plugin {
+    /** a unique identifier for your plugin */
     name: string;
+    /** extensions which should be resolved, including the leading period */
     extensions?: string[];
+    /** fileName patterns which should be resolved, excluding the trailing extension */
     fileNames?: string[];
     /** Executed before require/import of config file */
     register?(filePath: string): Promise<void>;
@@ -22,10 +48,23 @@ export interface Plugin {
 }
 
 interface Load<T extends Record<any, any> = Record<any, any>> {
+    /**
+     * @param namespace The namespace which will be searched for the configuration file.
+     * 
+     * For example, passing `"donut"` would resolve a files like `donut.config.js`, `donut.config.cjs`, and `donut.config.mjs` as well as a `package.json` with a `donut` property.
+     *
+     * @param opts Options to customize loader behavior
+     */
     (namespace: string, opts?: LoadOptions<T>): Promise<Config<T>>;
     use(plugins: Plugin[]): void;
 }
 
+/**
+ * An `async` function which searches for and loads a configuration file
+ *
+ * [Read the `@proload/core` docs](https://github.com/natemoo-re/proload/tree/main/packages/core#load)
+ */
 declare const load: Load;
 
+export = load;
 export default load;
