cd37d41888cb6508287d58c3573c2668e1dccbfb
Nate Moore <nate@skypack.dev>
2021-11-09 06:38:20 -0600

    fix: update jsonc-register

---
+++ .changeset/mighty-wolves-happen.md
@@ -0,0 +1,5 @@
+---
+"jsonc-register": minor
+---
+
+Update loading technique to strip JSON comments only
--- packages/jsonc-register/lib/index.cjs
+++ packages/jsonc-register/lib/index.cjs
@@ -1,10 +1,12 @@
 const { addHook } = require('pirates');
+const stripJsonComments = require('strip-json-comments');
 
 const DEFAULT_EXTENSIONS = ['.json', '.jsonc']
 
 function compile(sourcecode) {
     if (sourcecode.startsWith('module.exports')) return sourcecode;
-    return `module.exports = ${sourcecode}`;
+    const value = JSON.parse(stripJsonComments(sourcecode));
+    return `module.exports = ${JSON.stringify(value)}`;
 }
 
 function register() {
